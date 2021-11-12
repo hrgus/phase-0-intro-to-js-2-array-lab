2a62e1327bf3f42e2e348258a9bc2e724b077c3d
Harry <hrgus3@gmail.com>
2021-11-12 17:48:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+     
+    return cats.push(name);
+
+}
+destructivelyAppendCat('Ralph');
+
+
+
+function destructivelyPrependCat(name) {
+
+    return cats.unshift(name);
+
+}
+destructivelyPrependCat("Bob");
+
+
+
+function destructivelyRemoveLastCat() {
+
+    return cats.pop();
+
+}
+destructivelyRemoveLastCat();
+
+
+// I have to figure this one out. What are you doing wrong here?
+function destructivelyRemoveFirstCat() {
+
+    return cats.shift();
+
+}
+destructivelyRemoveFirstCat();
+
+
+// Needs to copy from "["Milo", "Otis", "Garfield"];"
+// To new Array "["Milo", "Otis", "Garfield", "Broom"];"
+function appendCat(name) {
+
+    const copyOfCats = [...cats, name];
+
+    return copyOfCats;
+
+}
+appendCat("Broom");
+
+
+
+function prependCat(name) {
+
+    const copyOfCats = [name, ...cats];
+
+    return copyOfCats;
+
+}
+prependCat("Arnold");
+
+
+
+function removeLastCat() {
+
+    const copyOfCats = cats.slice(0, -1);
+
+    return copyOfCats;
+
+}
+removeLastCat();
+
+
+
+function removeFirstCat() {
+
+    const copyOfCats = cats.slice(-2);
+    
+    return copyOfCats;
+
+}
+removeFirstCat();
